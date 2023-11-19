4257ce2597aa797ff31f18479c56681c43c6689e
CodeMahmoud <mahmoudcode913@gmail.com>
2023-11-18 18:09:57 -0600

    created Guess componenet for populating the grid with the user input

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import {range} from '../../utils'
+import {checkGuess} from '../../game-helpers'
+import {words} from '../../data'
 function Guess({value}) {
 
   return (
     <div className="guess-results">
  <p className="guess">
+  {value ? console.log(checkGuess(value)) : "hello"}
   {range(5).map(num => (
   <span className="cell" key={num}>{value ? value[num] : undefined}</span>
 
